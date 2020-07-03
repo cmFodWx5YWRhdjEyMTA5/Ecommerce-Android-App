d42656ef775bbd215ca8d002ce235b5bce4ee641
VAIBHAV HARIRAMANI <vaibhav.hariramani01@gmail.com>
2020-07-03 17:32:03 +0530

    Add files via upload

---
+++ Android App source code/vaibhav's Ecommerce app source code/app/src/main/java/com/vishal/store/WooBoxApp.kt
@@ -0,0 +1,89 @@
+package com.vishal.store
+
+import android.app.Dialog
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
+import com.google.android.gms.ads.MobileAds
+import com.vishal.store.network.RestApis
+import com.vishal.store.utils.Constants
+import com.vishal.store.utils.Constants.SharedPref.LANGUAGE
+import com.vishal.store.utils.LocaleManager
+import com.vishal.store.utils.SharedPrefUtils
+import com.vishal.store.utils.extensions.getSharedPrefInstance
+import com.onesignal.OneSignal
+import okhttp3.OkHttpClient
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig
+
+
+class WooBoxApp : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        appInstance = this
+        getSharedPrefInstance().apply {
+            appTheme = getIntValue(Constants.SharedPref.THEME, Constants.THEME.LIGHT)
+            language = getStringValue(LANGUAGE,"en")
+        }
+
+        // Set Custom Font
+        CalligraphyConfig.initDefault(
+                CalligraphyConfig.Builder().setDefaultFontPath(getString(R.string.font_regular)).setFontAttrId(
+                        R.attr.fontPath
+                ).build()
+        )
+
+        MobileAds.initialize(this) {}
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init()
+        OneSignal.clearOneSignalNotifications()
+    }
+
+
+    fun enableNotification(isEnabled: Boolean) {
+        OneSignal.setSubscription(isEnabled)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        localeManager = LocaleManager(base)
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    companion object {
+        lateinit var localeManager: LocaleManager
+        private lateinit var appInstance: WooBoxApp
+        var restApis: RestApis? = null
+        var okHttpClient: OkHttpClient? = null
+        var sharedPrefUtils: SharedPrefUtils? = null
+        var noInternetDialog: Dialog? = null
+        lateinit var language: String
+        var appTheme: Int = 0
+
+        fun getAppInstance(): WooBoxApp {
+            return appInstance
+        }
+
+        fun changeAppTheme(isDark: Boolean) {
+            getSharedPrefInstance().apply {
+                when {
+                    isDark -> setValue(Constants.SharedPref.THEME, Constants.THEME.DARK)
+                    else -> setValue(Constants.SharedPref.THEME, Constants.THEME.LIGHT)
+                }
+                appTheme = getIntValue(Constants.SharedPref.THEME, Constants.THEME.LIGHT)
+            }
+
+        }
+
+        fun changeLanguage(aLanguage: String) {
+            getSharedPrefInstance().setValue(LANGUAGE, aLanguage)
+            language = aLanguage
+        }
+
+
+    }
+}
